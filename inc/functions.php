b43e637ca23f249a420d3333fab5130be98500b0
horatiu84 <horatiul@yahoo.com>
2023-05-27 17:33:21 +0300

    checking errors

--- inc/functions.php
+++ inc/functions.php
@@ -7,4 +7,45 @@ function dd($text) {
     exit;
 }
 
-?>
+function validate($data, $rules) {
+    $errors = [];
+
+    foreach($rules as $input => $input_rules) {
+        $input_rules = explode('|',$input_rules);
+        foreach($input_rules as $rule) {
+            $split = explode(':', $rule);
+
+            switch($split[0]) {
+                case 'required':
+                    if((!isset($data[$input]) || !$data[$input]) || 
+                    (is_array($data[$input]) && isset($data[$input]['size']) && !$data[$input]['size']) ) {
+                        $errors[$input][] = beautifyInputName($input).' is required';
+                    }
+                    break;
+                    case 'match':
+                        if(!isset($data[$input]) || !isset($data[$split[1]]) || $data[$input] !== $data[$split[1]] )
+                         {
+                            $errors[$input][] = beautifyInputName($input).' and ' . beautifyInputName($split[1]) . ' must match';
+                        }
+                        break;
+                    case 'min' :
+                        if(!isset($data[$input]) || $data[$input] && strlen($data[$input])< $split[1] ) {
+                            $errors[$input][] = beautifyInputName($input).' is too short. Required at least '.$split[1].' characters!';
+                        }
+                        break;
+                        case 'max' :
+                            if(!isset($data[$input]) || $data[$input] && strlen($data[$input])> $split[1] ) {
+                                $errors[$input][] = beautifyInputName($input).' is too long. Maximum is '.$split[1].' characters allowed!';
+                            }
+                            break;
+
+            }
+        }
+    }
+
+    return $errors;
+}
+
+function beautifyInputName($input) {
+    return ucfirst(str_replace('_',' ', $input));
+}
--- scripts/register.php
+++ scripts/register.php
@@ -5,7 +5,16 @@ $errors = [];
 
 
 if(isset($_POST) && $_POST) {
-    
+    $rules = [
+        'username' => 'required',
+        'password' => 'required|min:5|max:10',
+        'confirm_password' => 'required|match:password',
+        'email' => 'required'
+    ];
+
+    $errors = validate($_POST,$rules);
+
+   
 }
 
 ?>
